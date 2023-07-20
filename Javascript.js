14d6b344b0c40853221128a3566b6f887f19deb2
PARAS SAXENA <parassaxenaofficial@gmail.com>
2023-07-20 19:25:39 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,96 @@
+// /*
+// >>  document.getElementById("TARGETED_ID_NAME").
+// >>  This will show you the whole block in which this ID is used.
+// >>  If we do like this [document.getElementById("TARGETED_ID_NAME").click()], 
+//     here [.click()] is a click command which automatically perform the click 
+//     action if we use this whole command in the console. 
+// */
+
+// alert("Alert Will Show a pop up \n window dialoge box");
+// // Here I have used [\n] to end the line and start the afterwords sentence from the next line.
+// document.write("Using this we can write inside the document.");
+// // But document.write is not recommended to use.
+// console.log("Hello Developer");
+// // This will allow us to Display the written text in the console (this is a option in the developer tool or inspect option).
+// // console.assert(4 == (6-2)) use this in console with different values give boolean answer.
+// console.warn("this is a warning");
+
+// Ways to Print in Javascript :
+// >> console.log("Hello World");
+// >> alert("me");
+// >> document.write("This is a document write");
+
+// 2. Javascript console API :
+// >> console.log("Hello World", 4 + 6, "Another Log");
+// >> console.warn("This Is A Warning");
+// >> console.error("This Is An Error");
+
+// 3. Javascript Variables : Containers to store data variables. 
+// >> var number1 = 34;
+// >> var number2 = 56;
+// >> console.log(number1 + number2);
+
+// 4. Data_Types in Javascript :
+// # String :
+// >> var str1 = "This Is A String";
+// >> var str2 = 'This Is Also A String';
+
+// # Numbers :
+// >> var num1 = 455;
+// >> var num2 = 56.76;
+
+// # Objects :
+// >> var marks = {
+//     Ravi: 34,
+//     Shubham: 78,
+//     Paras: 99.999,
+// };
+
+// # Booleans :
+// >> var a = true;
+// >> var b = false;
+
+// # Array :
+// >> var arr = [1,2,"bablu", 4,'Even a string'];
+
+// var und; 
+// [var und >> by default is now undefined since it carry no value]
+// var n = null;
+
+// At a very high level, there are two types of data_types in Javascript :
+// 1. Primitive : undefined, null, number, string, boolean, symbol;
+// 2. Reference : Arrays and Objects;
+ 
+// 5. Operators In Javascript :
+
+// # Arithmatic Operators : 
+// >> var a = 34;
+// >> var b = 56;
+// console.log("The Value Of [a + b] is : ", a+b);
+// console.log("The Value Of [a - b] is : ", a-b);
+// console.log("The Value Of [a * b] is : ", a*b);
+// console.log("The Value Of [a / b] is : ", a/b);
+
+// # Assignment Operators :
+// >> var c = b;
+// >> c+=b;
+// >> c-=b;
+// >> c*=b;
+// >> c/=b;
+// console.log(c);
+
+// # Comparision Operators :
+// var x = 34;
+// var y = 56;
+// console.log(x == y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x < y);
+// console.log(x > y);  
+
+// # Logical Operators :
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!false);
+
+ 
